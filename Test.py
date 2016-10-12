3c53cc1bb2729079c88c8a662237ee02ff066385
hardik_suthar <hsuthar326@gmail.com>
2016-10-12 19:24:50 +0530

    Change

--- Test.py
+++ Test.py
@@ -1,21 +1,3 @@
 import tensorflow as tf
-import numpy as np
-import scipy.io as sio
-
-data = sio.loadmat('/media/hardik/DataPart/Video1Features.mat')
-
-features = data['features']
-labels = data['frameLabels']
-
-n = features.shape[1]
-
-cnt = 0
-for i in range(n):
-    batch_X = features[0, i]
-    if (batch_X.shape[0] == 0):
-        continue
-    print(batch_X.shape)
-    cnt += 1
 
 
-print(cnt)
